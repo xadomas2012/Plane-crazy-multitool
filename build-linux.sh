0ea2d3611d92fc5d3c1b70499156d09ea244b078
xadomas2012 <lesniauskasadomas@gmail.com>
2026-08-31 14:11:26 +0300

    Make Linux build portable

--- build-linux.sh
+++ build-linux.sh
@@ -3,9 +3,9 @@ set -e
 
 mkdir -p dist
 
-go build \
-    -trimpath \
-    -ldflags="-s -w" \
-    -o dist/PC-Gear-Calculator-Linux-x64
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build \
+  -trimpath \
+  -ldflags="-s -w" \
+  -o dist/PC-Gear-Calculator-Linux-x64
 
 echo "Built: dist/PC-Gear-Calculator-Linux-x64"
